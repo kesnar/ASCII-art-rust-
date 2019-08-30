2fe9ba46de0ae632f4e4b81c7ad24413e9754c0b
kesnar <kesnar@gmail.com>
2019-08-30 14:07:46 +0300

    Reads image file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate image;
+
+use crate::image::GenericImageView;
+
 fn main() {
-    println!("Hello, world!");
+    let img = match image::open("res/3.jpg") {
+        Ok(a) => a,
+        Err(_) => panic!("Incorrect image file"),
+    };
+
+    let (width, height) = img.dimensions();
+
+    println!("Successfully loaded image!");
+    println!("Image size: {} x {}", width, height);
 }
